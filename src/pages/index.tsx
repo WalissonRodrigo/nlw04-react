e296cafe7f36d48da0e547d428fd1ff162d3e85c
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2021-02-26 15:34:09 -0300

    componente challengebox para mostrar os desafios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import styles from "../styles/pages/Home.module.css";
 import Profile from "../components/Profile";
 import CompletedChallenges from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
+import ChallengeBox from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -18,7 +19,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
